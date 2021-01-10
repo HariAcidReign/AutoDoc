c484fa338ad655e93ca910d73b8bebc69f833227
HariHaran <haran465@gmail.com>
2021-01-10 12:14:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -26,13 +26,6 @@ export default class App extends React.Component {
 		});
 		this.setState({ isFontLoaded: true });
 
-		// firebase.auth().onAuthStateChanged((user) => {
-		// 	if (user) {
-		// 		this.setState({ isLoggedIn: true });
-		// 	} else {
-		// 		this.setState({ isLoggedIn: false });
-		// 	}
-		// });
 	}
 	render() {
 		return this.state.isFontLoaded === true ? <AppNavigator /> : <AppLoading />;
